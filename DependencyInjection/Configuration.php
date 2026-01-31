3c3a2bd89ce4cbe092e5d1620b5626ef217ba91e
Adam Prager <prager.adam87@gmail.com>
2026-01-31 02:22:19 +0100

    Code upgrade PHP84

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,12 +17,10 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {		
-        $treeBuilder = new TreeBuilder('padam87_form_filter');
-
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
 
-        return $treeBuilder;
+        return new TreeBuilder('padam87_form_filter');
     }
 }
